16f77dd6455a06c60db0a5b9db558ab6598d217a
Erick Marques <67922086+E-rick23@users.noreply.github.com>
2024-07-18 20:27:53 -0300

    Update Main.java
    The code now asks for user input for the server address!

--- Mine_S/src/main/java/mineserver/Main.java
+++ Mine_S/src/main/java/mineserver/Main.java
@@ -1,9 +1,12 @@
 package mineserver;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Mine serverStatusChecker = new Mine();
-        String serverAddress = ""; // Put the server address here.
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Digite o endereço do servidor: ");
+        String serverAddress = keyboard.nextLine(); // Put the server address here.
         String status = serverStatusChecker.getServerStatus(serverAddress); //Stores the string with the status.
         System.out.println("Server Status: " + status);
     }
